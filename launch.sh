0ac327314b58ac244d31463daede12ef160654f3
thecoder-001 <ayush06feb@gmail.com>
2024-02-21 19:36:54 +0530

    Simplified launch script

--- launch.sh
+++ launch.sh
@@ -2,10 +2,5 @@
 
 # Small shell wrapper to assist with executing Endgame: Singularity
 
-DIR=/app/share/singularity
-PYTHON=${PYTHON:-python3}
-PYPATH=${DIR}${PYTHONPATH:+:${PYTHONPATH}}
-ls /app/share/
-#PYTHONPATH="${PYPATH}" "${PYTHON}" -m singularity "$@"
 cd /app/share/singularity-1.00
 python3 -m singularity
